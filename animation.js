b81c6e37c372adcd169616ba8695534a7f515ff3
KaushikKarambelkar <kaushik.iitmadras@gmail.com>
2017-04-13 17:44:15 +0530

    Animations added

--- animation.js
+++ animation.js
@@ -3,20 +3,20 @@ $(document).ready(function(){
 });
 
 function runtext() {
-	$("#text1").fadeIn(500, function(){
-		$("#text1").fadeOut(500, function(){
-			$("#text2").fadeIn(500, function(){
-				$("#text2").fadeOut(500, function(){
-					$("#text3").fadeIn(500, function(){
-						$("#text3").fadeOut(500, function(){
-							$("#text4").fadeIn(500, function(){
-								$("#text4").fadeOut(500, function(){
-									$("#text5").fadeIn(500, function(){
-										$("#text5").fadeOut(500, function(){
-											$("#text6").fadeIn(500, function(){
-												$("#text6").fadeOut(500, function(){
-													$("#text7").fadeIn(500, function(){
-														$("#text7").fadeOut(500, function(){
+	$("#text1").show(500, function(){
+		$("#text1").hide(1500, function(){
+			$("#text2").fadeIn(1000, function(){
+				$("#text2").fadeOut(1000, function(){
+					$("#text3").fadeIn(1000, function(){
+						$("#text3").fadeOut(1000, function(){
+							$("#text4").fadeIn(1000, function(){
+								$("#text4").fadeOut(1000, function(){
+									$("#text5").fadeIn(1000, function(){
+										$("#text5").fadeOut(1000, function(){
+											$("#text6").fadeIn(1000, function(){
+												$("#text6").fadeOut(1000, function(){
+													$("#text7").fadeIn(1000, function(){
+														$("#text7").fadeOut(1000, function(){
 															runtext();
 														});
 													});
